6409fdeaa03410c286465128bad0f203391831eb
e <sishengqikuo_xx@163.com>
2018-10-31 18:24:21 +0800

    2018-10-31 ...18:24

--- src/main/java/com/li/recommend/ZtkRecommendQuestions.scala
+++ src/main/java/com/li/recommend/ZtkRecommendQuestions.scala
@@ -8,15 +8,17 @@ object ZtkRecommendQuestions {
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf()
-//      .setMaster("local")
+//            .setMaster("local")
       .setAppName("RecommendQuestion")
+      .set("spark.reducer.maxSizeInFlight", "128m")
 
     val sc = new SparkContext(conf)
 
     /**
       * 1196836
       */
-    val collect = sc.broadcast(sc.textFile("hdfs://huatu70/ztk_question_record/v_question_user_cache_collect")
+    val collect = sc.broadcast(sc.textFile("hdfs://huatu70/ztk_question_record/v_question_user_cache_collect/*")
+      .coalesce(10)
       .mapPartitions {
         ite =>
           var arr = new ArrayBuffer[(String, Int)]()
@@ -32,7 +34,8 @@ object ZtkRecommendQuestions {
     /**
       * 28291890
       */
-    val finish = sc.broadcast(sc.textFile("hdfs://huatu70/ztk_question_record/v_question_user_cache_finish")
+    val finish = sc.textFile("hdfs://huatu70/ztk_question_record/v_question_user_cache_finish")
+      .coalesce(10)
       .mapPartitions {
         ite =>
           var arr = new ArrayBuffer[(String, Int)]()
@@ -44,42 +47,56 @@ object ZtkRecommendQuestions {
             arr += Tuple2(line(0).toInt + "-" + line(1).toInt, line(1).split(",").length)
           }
           arr.iterator
-      }.collectAsMap())
+      }
 
-
+    println(finish.count())
     /**
       * 30705910
       */
-    val isGrasp = sc.textFile("hdfs://huatu70/ztk_question_record/v_question_user_cache_wrong")
+    val wrong = sc.textFile("hdfs://huatu70/ztk_question_record/v_question_user_cache_wrong")
+      .coalesce(10)
       .mapPartitions {
         ite =>
-          var arr = new ArrayBuffer[(Int, (Int, Int))]()
-          val cast_collect = collect.value
-          val cast_finish = finish.value
+          var arr = new ArrayBuffer[(String, Int)]()
+
           while (ite.hasNext) {
             val next = ite.next()
 
             val line = next.split("_")
-
-            val userId = line(0).toInt
-            val question_point_id = line(1).toInt
-            val length = line(2).split(",").length
-
-            val collect_point_length = cast_collect.getOrElse(userId + "-" + question_point_id, 0)
-            val finish_point_length = cast_finish.getOrElse(userId + "-" + question_point_id, 0)
-
-
-            val wrongRate = length * 1.0 / (length + collect_point_length + finish_point_length)
-            if (wrongRate > 0.30) {
-              (userId, question_point_id, 0, wrongRate)
-            } else {
-              (userId, question_point_id, 1, wrongRate)
-            }
+            arr += Tuple2(line(0).toInt + "-" + line(1).toInt, line(1).split(",").length)
           }
-
           arr.iterator
       }
+    println(wrong.count())
+    //    finish.join(wrong).take(100).foreach(println)
+    finish.fullOuterJoin(wrong).mapPartitions {
+      ite =>
+        val arr = new ArrayBuffer[(String, String, Int, Double)]()
+        val coll = collect.value
 
-    isGrasp.saveAsTextFile("hdfs://huatu70/ztk_question_record/" + args(0))
+
+        while (ite.hasNext) {
+          val next = ite.next()
+          val user2Point = next._1
+          val fin = next._2._1.getOrElse(0)
+          val wro = next._2._2.getOrElse(0)
+          val col = coll.getOrElse(user2Point, 0)
+
+          if (fin + wro + col != 0) {
+            val isGrasp = wro * 1.0 / (fin + wro + col)
+            if (isGrasp > 0.30) {
+              arr += Tuple4(user2Point.split("-")(0), user2Point.split("-")(1), 1, isGrasp)
+            } else {
+              arr += Tuple4(user2Point.split("-")(0), user2Point.split("-")(1), 0, isGrasp)
+            }
+
+          } else {
+            arr += Tuple4(user2Point.split("-")(0), user2Point.split("-")(1), 0, 0.00)
+          }
+        }
+
+        arr.iterator
+    }.foreach(println)
+
   }
 }
